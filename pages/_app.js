379a3a6ab1aad8a7e2e2681ee66d9ed88d7450f7
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-01-04 11:36:04 +0200

    feat: project init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Materina clone" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
